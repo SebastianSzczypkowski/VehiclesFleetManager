55f4c03ac3c613d926059530ff71cd55c2f86d91
SebastianSzczypkowski <szczsebastian146@gmail.com>
2022-03-20 21:44:56 +0100

    widok mapa

---
+++ src/main/web/VehiclesFleetManagerFront/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MapComponent } from './componment/map/map.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+import {MatSelectModule} from "@angular/material/select";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {LeafletModule} from "@asymmetrik/ngx-leaflet";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MapComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatToolbarModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatInputModule,
+    LeafletModule,
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/main/web/VehiclesFleetManagerFront/src/app/componment/map/map.component.ts
@@ -0,0 +1,87 @@
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import * as L from 'leaflet';
+import {
+  Map,
+  latLng,
+  Control,
+  Layer,
+  MapOptions,
+  tileLayer,
+  ZoomAnimEvent,
+  LeafletEvent,
+  Icon,
+  Marker,
+  icon
+} from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import "leaflet-routing-machine";
+
+@Component({
+  selector: 'app-map',
+  // templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css'],
+  template: `<div class="map-container" leaflet
+     [leafletOptions]="options"
+     (leafletMapReady)="onMapReady($event)"
+     (leafletMapZoomEnd)="onMapZoomEnd($event)"
+    ></div>`,
+
+})
+export class MapComponent implements OnInit,OnDestroy {
+
+
+
+  @Output() map$: EventEmitter<Map> = new EventEmitter;
+  @Output() zoom$: EventEmitter<number> = new EventEmitter;
+  @Input() options: MapOptions= {
+    layers:[tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      opacity: 0.7,
+      maxZoom: 19,
+      detectRetina: true,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })],
+    zoom:12,
+   // center:latLng(0,0)
+    center: latLng(57.74, 11.94)
+  };
+  public map?: Map;
+  public zoom?: number;
+
+
+
+  constructor() {
+  }
+
+  private defaultIcon: Icon = icon({
+    iconUrl: "assets/marker-icon.png",
+    shadowUrl: "assets/marker-shadow.png"
+  });
+
+  ngOnInit() {
+     Marker.prototype.options.icon = this.defaultIcon;
+  }
+
+
+
+  ngOnDestroy() {
+    this.map?.clearAllEventListeners;
+    this.map?.remove();
+  };
+
+  onMapReady(map: Map) {
+    this.map = map;
+    this.map$.emit(map);
+    this.zoom = map.getZoom();
+    this.zoom$.emit(this.zoom);
+    L.Routing.control({
+      waypoints: [L.latLng(57.74, 11.94), L.latLng(57.6792, 11.949)],
+      routeWhileDragging: true
+    }).addTo(map);
+  }
+
+  onMapZoomEnd(e: LeafletEvent) {
+    this.zoom = e.target.getZoom();
+    this.zoom$.emit(this.zoom);
+  }
+}
